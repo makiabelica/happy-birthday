a131a42e87fa914380471c611168aba6a80f1608
arashiyouni <angie.espinoza.campos@gmail.com>
2024-07-30 15:41:10 -0600

    car happy birthday

--- app/src/main/java/com/example/happybirthday/MainActivity2.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity2.kt
@@ -1,70 +1,96 @@
 package com.example.happybirthday
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
-import androidx.compose.runtime.remember as remember
 
-class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val data = intent.getStringExtra("DATA")
             HappyBirthdayTheme {
+                BackgroundImage()
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Text(
-                        text = "Ingresa un nombre",
-                        fontSize = 40.sp,
+                        text = "FELI CUMPLEAÑOS 🥳",
+                        color = Color.White,
+                        fontSize = 37.sp,
+                        fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center
                     )
                     Spacer(modifier = Modifier.padding(30.dp))
-                    var text by remember {
-                        mutableStateOf("")
-                    }
-                    TextField(
-                        value = text,
-                        onValueChange = {text = it},
-                        label = { Text(text = "Nombre:")}
-                    )
-                    Spacer(modifier = Modifier.padding(30.dp))
-                    Button(onClick = {
-                        val intent = Intent(this@MainActivity, MainActivity2::class.java)
-                        intent.putExtra("DATA", text)
-                        startActivity(intent)
-                    }) {
-                        Text(text = "Enviar")
+                    Row {
+                        Text(
+                            data ?: "" ,
+                            color = Color.Magenta,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Bold
+                        )
                     }
                 }
+
             }
         }
     }
 }
-//TODO: HACERLO CON FUNCIONES UVU Y VER EL PREVIEW
+
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+
+    val image = painterResource(R.drawable.happybirthday)
+    Image(
+        painter = image,
+        contentDescription = null)
+
+}
+
+@Composable
+fun BackgroundImage() {
+    val image = painterResource(R.drawable.happybirthday)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingImage(
+            message = "Happy Birthday Sam!",
+            from = "From Emma"
+        )
+    }
+}
